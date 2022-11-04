15b9f88e20ffaa39db835daea240bd30fd809973
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-11-05 09:22:27 +1100

    add roots

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,134 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Blog, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// GET ALL blogs and ALL comments
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            where: {
+                // use the ID from the session
+                user_id: req.session.user_id,
+            },
+            attributes: ['id', 'title', 'content', 'date_created'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: [
+                        'id',
+                        'comment_text',
+                        'blog_id',
+                        'user_id',
+                        'date_created'
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ['username', 'github']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username', 'github']
+                },
+            ],
+        });
+
+        // Serialize data in array
+        const blogs = blogData.map(blog => blog.get({ plain: true }));
+
+        // Pass serialized data
+        res.render('dashboard', {
+            blogs,
+            loggedIn: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET a blog and edit
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.findOne({
+            where: {
+                id: req.params.id,
+            },
+            attributes: ['id', 'title', 'date_created', 'content'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: [
+                        'id',
+                        'comment_text',
+                        'blog_id',
+                        'user_id',
+                        'date_created',
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ['username', 'github'],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ['username', 'github'],
+                },
+            ],
+        });
+        if (!blogData) {
+            res.status(404).json({ message: 'No Blog found with this id' });
+            return;
+        }
+        // serialize
+        const blog = blogData.get({ plain: true });
+        res.render('edit-blog', {
+            blog,
+            loggedIn: true,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET Create blog
+router.get('/create/', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            where: {
+                // Read user id from session
+                user_id: req.session.user_id,
+            },
+            attributes: ['id', 'title', 'date_created', 'content'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: [
+                        'id',
+                        'comment_text',
+                        'blog_id',
+                        'user_id',
+                        'date_created',
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ['username', 'github'],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ['username', 'github'],
+                },
+            ],
+        });
+        const blogs = blogData.map(blog => blog.get({ plain: true }));
+        res.render('create-blog', {
+            blogs,
+            loggedIn: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
---
+++ controllers/homeRoute.js
@@ -0,0 +1,114 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Blog, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// FIND ALL blogs and comments and associated user_id
+router.get('/', async (req, res) => {
+  try {
+    console.log(req.session);
+    const blogData = await Blog.findAll({
+      attributes: ['id', 'title', 'date_created', 'content'],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_text',
+            'blog_id',
+            'user_id',
+            'date_created',
+          ],
+          include: {
+            model: User,
+            attributes: ['username', 'github'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['username', 'github'],
+        },
+      ],
+    });
+
+    // Serialize data in array
+
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    // Pass serialized data
+    res.render('homepage', {
+      blogs,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// User login
+router.get('/login', (req, res) => {
+  console.log(req.session);
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+// User signup
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup');
+});
+
+router.get('/blogs/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ['id', 'title', 'date_created', 'content', 'user_id'],
+
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_text',
+            'blog_id',
+            'user_id',
+            'date_created',
+          ],
+          include: {
+            model: User,
+            attributes: ['username', 'twitter'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['username', 'twitter'],
+        },
+      ],
+    });
+    if (!blogData) {
+      res.status(404).json({ message: 'No blog found with this id' });
+      return;
+    }
+
+    // serialize the data
+    const blog = blogData.get({ plain: true });
+
+    // pass data to template
+    res.render('single-comment', {
+      blog,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
