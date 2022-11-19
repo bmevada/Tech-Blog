0eaca5eeb32cd6bb45dd94527e3f9f57ffae90a1
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-11-19 21:29:07 +1100

    review functionality and added comments

--- models/User.js
+++ models/User.js
@@ -2,52 +2,62 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-//Password and Username
+//Create a user modeal - username and password
 class User extends Model {
     checkPassword(loginPw) {
         return bcrypt.compareSync(loginPw, this.password);
     }
 }
 
+// define table columns and configuration
 User.init(
     {
         id: {
+            // Define ID Column
+            // Sequelize DataTypes and state the type of data
+            // Instruct that this is the primary key
+            // Turn on auto increment
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true
         },
+        // Define a user column
         username: {
             type: DataTypes.STRING,
             allowNull: false
         },
-
+        // Define a password column
         password: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [3]
+                // Password must be at least 5 charcters long
+                len: [5]
             },
         },
 
     },
     {
+        // Set up beforeCreate hook functionality
         hooks: {
-            beforeCreate: async (newUserData) => {
+            async beforeCreate (newUserData) {
                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
                 return newUserData;
             },
-            beforeUpdate: async (updatedUserData) => {
-                updatedUserData.password = await bcrypt.hash(
-                    updatedUserData.password,
-                    10
-                );
+           // Set up beforeUpdate hook functionality 
+            async beforeUpdate (updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
                 return updatedUserData;
             },
         },
+        
+        // Pass in the imported sequelize connection in the database
         sequelize,
         freezeTableName: true,
+        // Use underscore insttead of camel casing
         underscored: true,
+        // Ensure the model remains in lower case
         modelName: 'user',
     }
 );
